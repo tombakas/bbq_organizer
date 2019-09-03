d8a1a84e600eb6605f6815c8905ced69391628c2
Tomas Tombakas <tombakas@gmail.com>
2019-09-03 15:35:09 +0200

    Reorganize settings further

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,21 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fgtj2pic#t92)4gq$%e1%k!&r$&d=0)2s4sb^&8&m01q1hju(&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -69,17 +57,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bbq_organizer.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,1 +1,13 @@
-from .base import *
+import os
+
+from .base import * # noqa
+
+SECRET_KEY = 'fgtj2pic#t92)4gq$%e1%k!&r$&d=0)2s4sb^&8&m01q1hju(&'
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa:F405
+    }
+}
