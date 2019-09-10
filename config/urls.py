1040f54699a90b582e624eebf69c2f27409b22a1
Tomas Tombakas <tombakas@gmail.com>
2019-09-10 23:33:52 +0200

    Initial login setup

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls import include
 
 from bbq_organizer import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home)
 ]
