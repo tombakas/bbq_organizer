8555d4d02e5350d12c2f6aaa72b08129926b70d7
Tomas Tombakas <tombakas@gmail.com>
2019-09-17 09:54:42 +0200

    Minor DEBUG django-environ variable fix

--- config/settings/dev_postgres.py
+++ config/settings/dev_postgres.py
@@ -2,7 +2,7 @@ from .base import * # noqa
 from .base import env
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG') == "True"
+DEBUG = env('DEBUG')
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 AUTH_PASSWORD_VALIDATORS = env("AUTH_PASSWORD_VALIDATORS", default=[])
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import env
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 STATIC_ROOT = env("STATIC_ROOT")
-DEBUG = env("DEBUG") == "True"
+DEBUG = env("DEBUG")
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
